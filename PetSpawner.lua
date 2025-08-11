cf01b5928fb687273166bbb7684eda9bdd0795fd
ren <ikazuryzel@gmail.com>
2025-08-11 14:13:16 +0800

    Update and rename Working PetSpawner.lua to PetSpawner.lua

---
+++ PetSpawner.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ranzurg/GrowaGarden/refs/heads/main/kyzel.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
--- Working PetSpawner.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
-task.wait(1)
-loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
