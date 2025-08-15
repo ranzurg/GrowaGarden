4878294b03e3990b7ceb1f9a850a2f9ba714a355
ren <ikazuryzel@gmail.com>
2025-08-15 20:20:30 +0800

    Update PetSpawner.lua

--- PetSpawner.lua
+++ PetSpawner.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ranzurg/GrowaGarden/refs/heads/main/kyzel.lua"))()
 task.wait(1)
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/3SSvzpPF3vLLWFOK/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/1gXaVhk1/raw"))()
